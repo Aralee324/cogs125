e1500b3416e4a708535cc37b8ef35e1e89b20204
Ara Lee <arl015@ucsd.edu>
2025-04-29 13:32:36 -0700

    Updates

---
+++ precache-manifest.c614485d16f7aaecdaef564806af4cb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1e47a45b9fff204bc03b21e69643f8",
+    "url": "/cogs125/index.html"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/cogs125/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/cogs125/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/cogs125/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/cogs125/static/js/main.fffdfd66.chunk.js"
+  },
+  {
+    "revision": "1f473602f952a05c44d6",
+    "url": "/cogs125/static/js/runtime~main.04152084.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/cogs125/static/media/logo.06e73328.svg"
+  }
+]);
